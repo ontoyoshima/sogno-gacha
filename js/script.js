b8c309212ba89974f2dde5aced0f8c4e10e1a654
takuto1529 <takutaku3230gudakusan@gmail.com>
2025-03-17 18:02:20 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// jsonファイルを取得し、jsの配列に変換する関数
+async function fetchMenu(){ // asyncは非同期関数を宣言。関数内でawaitが使える。awaitはこの処理が終わるまで次の処理をしない。
+    const response = await fetch("./menu.json"); // jsonの取得
+    const menuList = await response.json(); // jsonをjsの配列に変換
+    return menuList;
+}
+
+// 予算内で購入可能なメニューを取得する関数
+async function findAffordableMenus(budget){
+    const menuList = await fetchMenu();
+    return menuList.filter(item => item.price <= budget); // jsの各項目をitemという
+}
+
+// random pick up
+async function pickupMenu(budget){
+    let tray= [];
+    while(budget > 0){
+        var affordableMenus = await findAffordableMenus(budget); 
+        var menuNum = affordableMenus.length;
+        if (menuNum === 0) break;
+        var randomNum = Math.floor(Math.random() * menuNum);
+        tray.push(affordableMenus[randomNum]);
+        budget -= affordableMenus[randomNum].price;
+    }
+    return tray;
+}
+
+document.getElementById("priceForm").addEventListener("submit", async function(event){ // id=priceformの要素を取得、フォーム送信されるたびにfunction(event)を実行
+    event.preventDefault(); // continue action without page reload.通常フォームを送信するとページがリロードされる
+    const price = document.getElementById("price").value; // id=priceが付いてるinput要素を取得し、数値を取得
+    const tray = await pickupMenu(price);
+    console.log(tray);
+    let resultHTML = "<h2>選ばれたメニュー</h2><ul>";
+    var totalPrice = 0;
+    tray.forEach(menu => {
+        resultHTML += `<li>${menu.name}(${menu.price}円)</li>`; // `${変数}`で変数出力
+        totalPrice += menu.price;
+    });
+    resultHTML += `</ul><h3>合計${totalPrice}円</h3>`;
+    document.getElementById("result").innerHTML = resultHTML; // idが一致しているところのタグを取得。今回の場合<div>。innerHTMLでhtmlファイルのdivタグの中身書き換え。 
+})
